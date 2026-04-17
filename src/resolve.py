6788874c77d652d59625e97f7d3fad3a31965846
DavidKart <david@kartte.net>
2026-04-17 10:09:05 +0200

    org: move n_random_maps

--- src/resolve.py
+++ src/resolve.py
@@ -229,20 +229,6 @@ def process_one_file(job: ProcessingJob) -> tuple[str, float, float] | None:
 		print("to measure resolutions [Å]: " + " ".join(map(str,np.round(resolutions,2))))
 		print("")
 
-	# # How many random maps are needed to get a good enough reference distribution
-	# maxWindow_test = int(np.ceil(np.max(windows)))*2+1
-	# maxWindow_entries = np.prod([maxWindow_test for _ in range(dimension)])	
-	# map_volume  = np.prod(np.array(sizeMap)+maxWindow_test)
-	# if job.config == "Tilt-Series":
-	# 	maxWindow_entries = np.prod([maxWindow_test for _ in range(dimension_windows)])	
-	# 	map_volume  = np.prod(np.array(sizeMap[1:]) + maxWindow_test)
-	# 	possibleTests_nonOverlapping = int(map_volume / maxWindow_entries)
-	# 	possibleTests_nonOverlapping = possibleTests_nonOverlapping **2 # Consider enhanced possibility space due to independent real-space locations between permutation half-maps 
-	# else:
-	# 	possibleTests_nonOverlapping = int(map_volume / maxWindow_entries)
-	# 	possibleTests_nonOverlapping = possibleTests_nonOverlapping **2 # Consider enhanced possibility space due to independent real-space locations between permutation half-maps 
-	# n_randomMaps = int(np.ceil(job.referenceDistSize / possibleTests_nonOverlapping))
-
 	# Preparation as batches
 	if job.config == "Tilt-Series":
 		batchHalf1 = [torch.from_numpy(halfMap1Data[i].astype(np.float32)).to(device) for i in range(halfMap1Data.shape[0])]
